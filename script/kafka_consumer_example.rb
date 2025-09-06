cd7f9a10e8e3401de2a456ea35f4441c25607d12
Shareef Dudekula <shareef.ddkl786@gmail.com>
2025-09-06 20:14:16 +0530

    fixed zeitwork issue

--- script/kafka_consumer_example.rb
+++ script/kafka_consumer_example.rb
@@ -1,7 +1,7 @@
 # Usage example for KafkaConsumerService
-# You can run this script with: rails runner app/services/kafka_consumer_example.rb
+# You can run this script with: rails runner script/kafka_consumer_example.rb
 
-require_relative 'kafka_consumer_service'
+# No need for require_relative since KafkaConsumerService is in app/services and will be autoloaded
 
 topic = 'books'
 group_id = 'books-consumer-group'
